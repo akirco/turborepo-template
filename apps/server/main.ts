5507c1214e2c29eea5c0bac0654a246347900d1c
akirco <1833708553@qq.com>
2023-09-22 12:28:38 +0800

    upload template

---
+++ apps/server/main.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const app = express();
+const httpServer = http.createServer(app);
+const io = new Server(httpServer);
+
+io.on('connection', () => {
+  //
+});
+
+httpServer.listen(process.env.PORT || 8000, () =>
+  console.log('Server is running,check out:http://localhost:8000')
+);
